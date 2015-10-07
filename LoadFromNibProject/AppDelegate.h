c3a31205e1b79d86b2fe5789527147dcde91a836
Ersen Tekin <ersen.tekin@hepsiburada.com>
2015-10-07 13:48:16 +0300

    Initial Commit

---
+++ LoadFromNibProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoadFromNibProject
+//
+//  Created by Ersen Tekin on 07/10/15.
+//  Copyright © 2015 Ersen Tekin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
